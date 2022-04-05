37b729156c981685560d862df41c298b24912c3a
Brandon Rodriguez <devbrod92@gmail.com>
2022-04-04 22:31:08 -0500

    :new: Header component jsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./Header.css";
+import Cta from "./Cta";
+import Me from "../../assets/me.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Kanao Tsuyuri</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <Cta />
+
+        <div className="me">
+          <img src={Me} alt="me" />
+        </div>
+
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
